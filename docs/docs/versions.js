2d95125de8cf90108686ab05759afe61ab25d500
citkane <citkane@users.noreply.github.com>
2022-08-04 12:06:57 +0000

    Deploying to gh-pages from @ citkane/typedoc-plugin-versions@143dba1451b150197ec2eff275a04b15556ca6fa 🚀

---
+++ docs/docs/versions.js
@@ -0,0 +1,10 @@
+
+"use strict"
+
+export const DOC_VERSIONS = [
+	'stable',
+	'v2.0',
+	'v0.1',
+	'v0.0',
+	'dev'
+];
--- docs/versions.js
+++ docs/versions.js
@@ -3,8 +3,6 @@
 
 export const DOC_VERSIONS = [
 	'stable',
-	'v2.0',
-	'v0.1',
 	'v0.0',
 	'dev'
 ];
