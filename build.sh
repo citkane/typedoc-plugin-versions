a26d8c1f76f80f0bd01f4a818ce2fd59aeb14421
citkane <michael@openpoint.ie>
2022-08-23 18:45:45 +0100

    Change build script to .js for windows dev

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-./node_modules/.bin/tsc;
-cp ./README.md ./dist/README.md;
-cp ./LICENSE.txt ./dist/LICENSE.txt;
-cp ./.npmignore ./dist/.npmignore;
-cp ./package.json ./dist/package.json;
-mkdir -p ./dist/src/assets/;
-cp ./src/assets/versionsMenu.js ./dist/src/assets/versionsMenu.js;
